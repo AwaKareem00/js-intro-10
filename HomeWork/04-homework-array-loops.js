4ecc4e1b644549646fca3d9ae6aa0f248efdb7fc
“AwaKareem00” <Awa.kareem00@gmail.com>
2024-08-27 17:20:20 -0500

    Pushing edited code

--- HomeWork/04-homework-array-loops.js
+++ HomeWork/04-homework-array-loops.js
@@ -1,3 +1,5 @@
+const { getRandomNumber} = require('../utils/MathHelper.js')
+
 console.log('\n-------------------TASK01---------------------\n');
 for(let i = 1; i <= 100; i++) {
     if(i % 7 === 0) console.log(i);
@@ -21,16 +23,33 @@ for(let i = 0; i <= 10; i++) {
 }
 console.log(sum);
 console.log('\n-------------------TASK06---------------------\n');
+let ranNum = getRandomNumber(1, 10)
+let factorial = ranNum;
 
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
-console.log('\n-------------------TASK01---------------------\n');
+
+for (let i = ranNum - 1; i > 1; i--) {
+    factorial *= (i);
+}
+if(factorial === 0) factorial = 1;
+console.log(factorial);
+console.log('\n-------------------TASK07---------------------\n');
+let attempts = 0;
+let ranNum1;
+
+do {
+    ranNum1 = getRandomNumber(1, 100);
+    attempts++;
+}while(ranNum1 % 5 !== 0);
+
+console.log(`The random number is ${ranNum1} and it took ${attempts} attempt to generate it`);
+console.log('\n-------------------TASK08---------------------\n');
+console.log('\n-------------------TASK09---------------------\n');
+console.log('\n-------------------TASK10---------------------\n');
+console.log('\n-------------------TASK11---------------------\n');
+console.log('\n-------------------TASK12---------------------\n');
+console.log('\n-------------------TASK13---------------------\n');
+console.log('\n-------------------TASK14---------------------\n');
+console.log('\n-------------------TASK15---------------------\n');
 console.log('\n-------------------TASK01---------------------\n');
 console.log('\n-------------------TASK01---------------------\n');
 console.log('\n-------------------TASK01---------------------\n');
