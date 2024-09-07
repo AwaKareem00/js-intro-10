1d68dbd2aaeb0aa7a6d33d9e2a7f894f4bdb8d76
“AwaKareem00” <Awa.kareem00@gmail.com>
2024-09-07 12:55:51 -0500

    push Code

---
+++ js-miscellaneous/09-hoisting.js
@@ -0,0 +1,28 @@
+
+
+
+// printHi(); // Hi
+
+// function expreion
+const printHi = () => {
+    console.log('Hi');
+}
+
+printHi(); // Hi
+
+
+// variable hoisting
+console.log(fname); // undefined 
+var fname = 'john'
+
+/*
+var fname;
+console.log(fname); // undefined
+
+fname = 'johm'
+*/
+
+
+
+
+
