45dc441066c18a95a0eef27b4d7d42f6e6aaadcc
“AwaKareem00” <Awa.kareem00@gmail.com>
2024-09-14 17:08:12 -0500

    push'

--- 14-async/01-intro.js
+++ 14-async/01-intro.js
@@ -1,24 +1,33 @@
-// sync
+// Synchronous code
 console.log('1');
 console.log('2');
 console.log('3');
 
 setTimeout(() => {
-    console.log('A');
+  console.log('A');
 }, 2000);
 
 setTimeout(() => {
-    console.log('B');
-}, 1000)
+  console.log('B');
+}, 1000);
 
 setTimeout(() => {
-    console.log('C');
-}, 1500)
+  console.log('C');
+}, 1500);
 
-// what we expect 
+// OUTPUT: 
 /*
+What we expect
 A
 B
 C
+and takes 4.5 seconds
+*/
 
+/*
+What actually happens
+B
+C
+A
+2 seconds
 */
--- 14-async/02-callbacks.js
+++ 14-async/02-callbacks.js
@@ -1,34 +1,52 @@
 const { Cypress } = require('../utils/cypress.js');
-
-
-// async way - PROBLEM
-// Cypress.visit('www.home.com');
-// Cypress.login('kareem', 'awadallah');
-// Cypress.validateLoginMessage('You are logged in');
-
+// Asynchronous way - PROBLEM!
+/*
+Cypress.visit('www.techglobal-training.com');
+Cypress.login('TechGlobal', 'Test1234');
+Cypress.validateLoginMessage('You are logged in');
+*/
 /*
 PROBLEM
 You are logged in validated!
-Logged in with kareem awadallah!
-www.home.com visited!
-and it takes 1 second
+Logged in with TechGlobal Test1234!
+www.techglobal-training.com visited!
+and it takes around 1 second
 
 EXPECTED:
-www.home.com visited!
+www.techglobal-training.com visited!
+Logged in with TechGlobal Test1234!
 You are logged in validated!
-Logged in with kareem awadallah!
-and it takes around 2 seconds
+and it takes around 2 takes.
 */
 
-//using same methods with callbacks added
-// Cypress.visit1('www.home.com', () => {
-//     Cypress.login1('Tech1', 'Tech2', () => {
-//         Cypress.validateLoginMessage1('You are logged in!');
-//     })
-// });
+// Using same methods with callbacks added 
+// Cypress.visit1('www.techglobal-training.com', () => {
+//   Cypress.login1('TechGlobal', 'Test1234', () => {
+//     Cypress.validateLoginMessage1('You are logged in');
+//   })
+// })
 
-// using promises -> then motheds
 
-Cypress.visit2('www.home.com')
-    .then(() => Cypress.login2('Tech1', 'tech2'))
-    .then(() => Cypress.validateLoginMessage2('You are logged in!'));
+// Using same methods with promises added -> using then 
+// without function
+// Cypress.visit2('www.techglobal-training.com')
+//   .then(() => Cypress.login2('TechGlobal', 'Test1234'))
+//   .then(() => Cypress.validateLoginMessage2('You are logged in'));
+
+  // with functions
+  function testValidLogin1() {
+    Cypress.visit2('www.techglobal-training.com')
+      .then(() => Cypress.login2('TechGlobal', 'Test1234'))
+      .then(() => Cypress.validateLoginMessage2('You are logged in'));
+  }
+  
+  testValidLogin1();
+  
+  // ASYNC-AWAIT - Playwright
+  async function testValidLogin2() {
+    await Cypress.visit2('www.techglobal-training.com');
+    await Cypress.login2('TechGlobal', 'Test1234');
+    await Cypress.validateLoginMessage2('You are logged in');
+  }
+  
+  testValidLogin2();
