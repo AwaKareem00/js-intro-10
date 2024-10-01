b393f61867190c1edd048c0c4bf29eaa1af62c50
“AwaKareem00” <Awa.kareem00@gmail.com>
2024-10-01 16:49:30 -0500

    Push Code

--- 11-js-array-callbacks/08-map-reduce-filter.js
+++ 11-js-array-callbacks/08-map-reduce-filter.js
@@ -7,7 +7,7 @@ const arr = [ el1, el2, el3, el4, el5, el6 ];
 
 
 count, sum, average, min, max, shortest, longest, most expensive simply result being one simple result -> reduce method
-returning some of the elements (subarray of the original) -> filter method
+returning some of the elements (subarray of the original) ->  method
 returning a same size new array with modified original array elements -> map
 */
 
